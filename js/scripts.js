0f0941b3b0784da2bea64a944ec7bb75bda4e078
Zach Holz <zach.a.holz@gmail.com>
2016-06-25 11:25:56 -0700

    Add all css styling and Forrest Gump gif

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@
 var playPingPong = function(number) {
   for(i = 1; i <= number; i +=1) {   
     if(i % 15 === 0) {
-      $("ul#counting").append("<li>pingpong</li>") 
+      $("ul#counting").append("<li id='pingpong'>pingpong</li>") 
     } else if(i % 5 === 0) {
-      $("ul#counting").append("<li>pong</li>") 
+      $("ul#counting").append("<li id='pong'>pong</li>") 
     } else if(i % 3 === 0) {
-      $("ul#counting").append("<li>ping</li>") 
+      $("ul#counting").append("<li id='ping'>ping</li>") 
     } else {
       $("ul#counting").append("<li>" + i + "</li>") 
     }
