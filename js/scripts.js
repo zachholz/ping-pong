ad5a7962f0ec19cb8d23e54330ed9271df8ca502
Zach Holz <zach.a.holz@gmail.com>
2016-06-25 09:36:10 -0700

    :sparkles: Pass specs three and four and refactor if else statements

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,12 @@
 
 var playPingPong = function(number) {
   for(i = 1; i <= number; i +=1) {   
-    if(i % 3 === 0) {
+    if(i % 15 === 0) {
+      $("ul#counting").append("<li>pingpong</li>") 
+    } else if(i % 5 === 0) {
+      $("ul#counting").append("<li>pong</li>") 
+    } else if(i % 3 === 0) {
       $("ul#counting").append("<li>ping</li>") 
-//     } else if() {
-//       // code for "pong"
-//     } else if() {
-//       // code for "pingpong"
     } else {
       $("ul#counting").append("<li>" + i + "</li>") 
     }
