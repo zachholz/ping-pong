2cdb3a36875e65438f61eb155593cce302c89e52
Zach Holz <zach.a.holz@gmail.com>
2016-06-25 07:54:29 -0700

    Add initial business logic and user interface logic is js

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// business logic
+
+var playPingPong = function(input) {
+  for()
+}
+  
+
+// user interface logic
+$(document).ready(function() {
+  $("input").submit(function(event) {
+  var number = parseInt($("input").val());
+  playPingPong(input);
+  
+  
+  event.preventDefault();
+  
+  });
+});
